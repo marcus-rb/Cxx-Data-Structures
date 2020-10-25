4dee0f7f6729de61e21f1eb8050ddd83a9aa0dd3
Marcus Benrud <marcus.benrud@gmail.com>
2020-10-25 21:42:03 +0100

    more work done on forward_list class, operator== declared

--- class/forward_list.h
+++ class/forward_list.h
@@ -1,5 +1,13 @@
 #pragma once
 
+// CLASS SETUP
+/* [ ] constructor
+ * [ ] destructor
+ * [ ] operator=
+ * [ ] assign
+ * 
+ */
+
 #include "..\core\custom_def.h"
 #include "..\utility\iterator.h"
 #include "..\utility\memory.h"
@@ -11,6 +19,8 @@ class forward_list_iterator : public base_forward_iterator<typename flist_type::
 public:
 	using T_ptr = typename flist_type::T_ptr;
 	using T_ref = typename flist_type::T_ref;
+
+	using value_type = typename flist_type::value_type;
 };
 
 template <typename T, typename alloc = allocator<T>>
@@ -21,9 +31,26 @@ public:
 	using T_ptr = T*;
 	using T_ref = T&;
 
+	using const_T_ptr = const T*;
+	using const_T_ref = const T&;
+
 	using value_type = T;
 private:
-	
+	class list_node;
+
+	list_node* m_head;
+	list_node* m_tail;
+public:
+
+	forward_list() : m_head(nullptr), m_tail(nullptr) noexcept;
+
+	// --- ELEMENT ACCESS ---
+
+	// --- ITERATORS ---
+
+	// --- MODIFIERS ---
+
+private:
 	class list_node {
 		T_ptr m_list_data;
 		list_node* m_next;
@@ -31,3 +58,8 @@ private:
 };
 
 _CUSTOM_END_
+
+template <typename T, typename alloc = custom::allocator<T>>
+bool operator==(custom::forward_list<T, alloc>& lhs, custom::forward_list<T, alloc>& rhs) {
+
+}
