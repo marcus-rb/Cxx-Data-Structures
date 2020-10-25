085fdc3ccefce97abaf12f97482617399421f13f
Marcus Benrud <marcus.benrud@gmail.com>
2020-10-25 21:45:20 +0100

    added swap function template, and declared specializations for vector and array

--- utility/utility_core.h
+++ utility/utility_core.h
@@ -9,4 +9,19 @@ size_t max(const T& left, const T& right) {
 	return left < right ? right : left;
 }
 
+template <typename T>
+void swap(T& a, T& b) {
+	T temp = a;
+	a = b;
+	b = temp;
+}
+
+#if false
+template<typename T>
+void swap<custom::vector<T>>(custom::vector<T>& a, custom::vector<T>& b);
+
+template<typename T, size_t N>
+void swap<custom::array<T, N>>(custom::array<T, N>& a, custom::array<T, N>& b);
+#endif
+
 _CUSTOM_END_ 
